68088cec379a4e476ba817a4838513df4b5e17fb
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2021-01-28 02:25:54 -0300

    Finish study of Java Time API.G

---
+++ ocajp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+g
+7src/br/com/jse/primitive/PrimitiveTypesExplanation.java,5\b\5b4a6e5f13de00df70cdb228aad021b67c2bb763
+S
+#src/br/com/jse/dates/DateTypes.java,c\6\c6b8d7b1adefc0d2ef4e3e87611b7966c7bead6e
--- ocajp/src/br/com/jse/dates/DateTypes.java
+++ ocajp/src/br/com/jse/dates/DateTypes.java
@@ -1,7 +1,11 @@
 package br.com.jse.dates;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateTypes {
     public static void main(String[] args) {
@@ -46,6 +50,94 @@ public class DateTypes {
         System.out.println(fullChristmas.get(ChronoField.YEAR_OF_ERA));
         System.out.println(fullChristmas.get(ChronoField.HOUR_OF_DAY));
         System.out.println(fullChristmas.get(ChronoField.MINUTE_OF_HOUR));
+        System.out.println();
+
+        System.out.println("-- Comparing Dates --");
+
+        MonthDay d1 = MonthDay.of(1,1);
+        MonthDay d2 = MonthDay.of(1,2);
+
+        System.out.println(d1.isBefore(d2));
+        System.out.println(d2.isBefore(d1));
+        System.out.println(d2.isAfter(d1));
+        System.out.println(d1.isAfter(d2));
+        System.out.println(d1.equals(d2));
+        System.out.println();
+
+        System.out.println("-- Assigning --");
+        LocalDate oldDate = LocalDate.of(2016,4,4);
+        LocalDate neoDate = oldDate.withDayOfMonth(3);
+
+        System.out.println(oldDate);
+        System.out.println(neoDate);
+        System.out.println(oldDate.plusDays(27));
+        System.out.println(oldDate.plusYears(5));
+        System.out.println(oldDate.minus(5, ChronoUnit.YEARS));
+        System.out.println("");
+
+        System.out.println("-- Conversion of Dates --");
+        LocalDateTime dateTime = LocalDateTime.now();
+        LocalDate date = dateTime.toLocalDate();
+        System.out.println(dateTime);
+        System.out.println(date);
+        System.out.println(dateTime.toLocalTime());
+
+        LocalDateTime recoverDateTimeByDate = date.atTime(dateTime.toLocalTime());
+        LocalDateTime recoverDateTimeByTime = dateTime.toLocalTime().atDate(date);
+        System.out.println(recoverDateTimeByDate);
+        System.out.println(recoverDateTimeByTime);
+        System.out.println();
+
+        System.out.println("-- Parsing Old Java Date API to a new API Java Time");
+        Date d = new Date();
+        Instant i = d.toInstant(); //milliseconds since 01/01/1970
+        LocalDateTime dateByOldJavaDateAPI = LocalDateTime.ofInstant(i, ZoneId.systemDefault());
+        System.out.println(dateByOldJavaDateAPI);
+
+        Calendar calendar = Calendar.getInstance();
+        Instant instant = calendar.toInstant();
+        LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        System.out.println(ldt);
+
+        Instant utc = localDateTime.toInstant(ZoneOffset.UTC);
+        System.out.println(String.format("ZoneOffset.UTC %S", utc));
+        System.out.println("");
+
+        System.out.println("-- Operations with Dates --");
+        Instant i1 = Instant.now();
+        Instant i2 = i1.plus(Duration.ofSeconds(10));
+        System.out.println(i1);
+        System.out.println(i2);
+
+        Instant epoch1970 = Instant.EPOCH;
+        Instant now = Instant.now();
+
+        Long secondsBetween1970andNow = Duration.between(epoch1970, now).getSeconds();
+        System.out.println(secondsBetween1970andNow);
+
+        LocalDateTime birthDay = LocalDateTime.of(1990,12,26,12,0);
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(ChronoUnit.YEARS.between(birthDay, today));
+        System.out.println(ChronoUnit.MONTHS.between(birthDay, today));
+        System.out.println(ChronoUnit.DAYS.between(birthDay, today));
+        System.out.println(ChronoUnit.HOURS.between(birthDay, today));
+        System.out.println(ChronoUnit.MINUTES.between(birthDay, today));
+        System.out.println(ChronoUnit.SECONDS.between(birthDay, today));
+
+        Period lifeTime = Period.between(birthDay.toLocalDate(), today.toLocalDate());
+        System.out.println(lifeTime.getYears());
+        System.out.println(lifeTime.getMonths());
+        System.out.println(lifeTime.getDays());
+        System.out.println();
+
+        System.out.println("-- Format Dates --");
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatterFullTime = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss:mm");
+        System.out.println(formatter.format(currentDate));
+        System.out.println(formatterFullTime.format(currentDate));
+        System.out.println(LocalDate.parse("28/12/2021", formatter));
     }
     //Date without hour
     LocalDate localDate;
