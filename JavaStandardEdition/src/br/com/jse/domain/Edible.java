1c93c833de2016ac38f947d04ebd2fb86b448a4f
Bryan Carlos Duarte Albuquerque <bryan.duarte@sensedia.com>
2021-04-04 01:15:10 -0300

    Update base project.

--- JavaStandardEdition/src/br/com/jse/domain/Edible.java
+++
@@ -1,4 +0,0 @@
-package br.com.jse.domain;
-
-public interface Edible {
-}
---
+++ ocajp/src/br/com/jse/primitive/PrimitiveTypes.java
@@ -0,0 +1,32 @@
+package br.com.jse.primitive;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+        args.length
+
+        byte b = 99; //0-127 default value 0
+
+        short s =  9999;//-32768-32767 default value 0
+
+        int i  = 999999999;
+
+        float f = 1.345678999f;
+
+        double d =  1.56;
+
+        long l = 999999999;
+
+        char c = 'a';
+
+        boolean bool = false;
+
+        c = 33;
+        b = 127;
+        s = 32767;
+
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
